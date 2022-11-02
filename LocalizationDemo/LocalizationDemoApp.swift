2776fec118df4cdb4265efd2faf7da26c8a27f54
Davide Troise <davide@troise.net>
2022-11-02 17:09:44 +0100

    Initial Commit

---
+++ LocalizationDemo/LocalizationDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  LocalizationDemoApp.swift
+//  LocalizationDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct LocalizationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
